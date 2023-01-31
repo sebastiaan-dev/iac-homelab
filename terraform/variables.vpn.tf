96878d8904c4558d7fc2f34359a6a6fc50d8fa41
Sebastiaan Gerritsen <s.e.gerritsen@outlook.com>
2023-01-31 17:22:47 +0100

    Implement VPN module

---
+++ terraform/variables.vpn.tf
@@ -0,0 +1,27 @@
+#
+# VPN service configuration
+#
+
+# VM
+
+variable "vpn_vm_cpus" {
+  type = string
+}
+
+variable "vpn_vm_memory" {
+  type = string
+}
+
+variable "vpn_vm_disk_size" {
+  type = string
+}
+
+variable "vpn_vm_hostname" {
+  type = string
+}
+
+variable "vpn_vm_ipv4_address" {
+  type = string
+}
+
+# Service
